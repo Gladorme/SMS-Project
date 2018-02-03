0e088aed740a2474ea4ed06d71d1457721fdf20f
Unknown <mistersanglad@gmail.com>
2018-02-03 11:22:22 +0100

    Rajout lib SQL: jdbc

--- SMS-Project-Client/src/fr/rt/sms/utils/SQLite.java
+++ SMS-Project-Client/src/fr/rt/sms/utils/SQLite.java
@@ -1,9 +1,5 @@
 package fr.rt.sms.utils;
 
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 public class SQLite {
 
     public static void main(String[] args) {
